c5604fd43d6307b34ebe6a26adb2dcd465a4934e
David Hirtle <david.hirtle@grammarly.com>
2021-01-06 18:10:16 -0800

    Clean up react-slate demo

--- examples/react-slate/src/Editor.js
+++ examples/react-slate/src/Editor.js
@@ -4,42 +4,14 @@ import { withHistory } from "slate-history";
 import { Slate, withReact } from "slate-react";
 
 import { GrammarlyEditable, GrammarlySlate } from "@grammarly/react-slate";
-
-/** @type {import('slate').Node[]} */
-const initialValue = [
-  {
-    children: [
-      { text: "The basics\n\n" },
-      {
-        text:
-          "Mispellings and grammatical errors can effect your credibility. The same goes for misused commas, and other types of punctuation . Not only will Grammarly underline these issues in red, it will also showed you how to correctly write the sentence.\n\n\n",
-      },
-      { text: "But wait...there's more?\n\n" },
-      {
-        text:
-          "Blue underlines mean a clarity issue has been spotted by Grammarly. You'll find suggestions that can possibly help you revise a wordy sentence in an effortless manner.\n\n",
-      },
-      {
-        text:
-          "Grammarly will also inspect your vocabulary carefully and suggest the best word with green underlines to make sure you don't have to analyze your writing too much.\n\n",
-      },
-      {
-        text:
-          "Furthermore, Grammarly can check for unoriginal content. Any sufficiently advanced technology is indistinguishable from magic. Norway maintains the Nordic welfare model with universal health care and a comprehensive social security system, and its values are rooted in egalitarian ideals.\n\n",
-      },
-    ],
-  },
-];
+import { demoApiKey, demoText } from "./demo";
 
 export const Editor = () => {
-  const [value, setValue] = useState(initialValue);
+  const [value, setValue] = useState(demoText);
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   return (
     <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
-      <GrammarlySlate
-        value={value}
-        apiKey="5c891c34-55b1-4504-b1a2-5215d35757ba"
-      >
+      <GrammarlySlate value={value} apiKey={demoApiKey}>
         <div style={{ padding: "1rem" }}>
           <GrammarlyEditable placeholder="Enter some plain text..." />
         </div>
---
+++ examples/react-slate/src/demo.js
@@ -0,0 +1,23 @@
+export const demoApiKey = "d7c1f3b5-12de-4b97-9cc3-4b2342c6e2d5";
+
+/** @type {import('slate').Node[]} */
+export const demoText = [
+  {
+    children: [
+      { text: "The basics\n\n" },
+      {
+        text:
+          "Mispellings and grammatical errors can effect your credibility. The same goes for misused commas, and other types of punctuation . Not only will Grammarly underline these issues in red, it will also showed you how to correctly write the sentence.\n\n\n",
+      },
+      { text: "But wait...there's more?\n\n" },
+      {
+        text:
+          "Blue underlines mean a clarity issue has been spotted by Grammarly. You'll find suggestions that can possibly help you revise a wordy sentence in an effortless manner.\n\n",
+      },
+      {
+        text:
+          "Grammarly will also inspect your vocabulary carefully and suggest the best word with green underlines to make sure you don't have to analyze your writing too much.\n\n",
+      },
+    ],
+  },
+];
