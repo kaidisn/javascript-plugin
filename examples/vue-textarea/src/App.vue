37dae22f571ab920ac6b095b5d7e7d0f2d0f5a73
David Hirtle <david.hirtle@grammarly.com>
2021-01-07 20:26:25 -0800

    Add vue-textarea example

---
+++ examples/vue-textarea/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Editor />
+  </div>
+</template>
+
+<script>
+import Editor from "./components/Editor";
+
+export default {
+  name: "App",
+  components: {
+    Editor
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+textarea {
+  width: 100%;
+  padding: 16px;
+  line-height: 1.4;
+}
+</style>
---
+++ examples/vue-textarea/src/components/Editor.vue
@@ -0,0 +1,13 @@
+<script>
+import { GrammarlyTextArea } from "@grammarly/vue-textarea";
+import { demoText } from "./demo";
+
+export default {
+  data: () => ({ text: demoText }),
+  components: { GrammarlyTextArea },
+};
+</script>
+
+<template>
+  <GrammarlyTextArea v-model="text" apiKey="5c891c34-55b1-4504-b1a2-5215d35757ba" />
+</template>
